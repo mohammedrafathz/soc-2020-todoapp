04a927c3d81cb81a80788fa4b6eb102e2a85f507
mohammedrafathz <mohammedrafathz@gmail.com>
2020-07-16 16:55:51 +0530

    added strikeoff feature

--- todo.js
+++ todo.js
@@ -2,7 +2,48 @@ var todoInput = document.getElementById("todoInput");
 var btnAdd = document.getElementById("btnAdd");
 var todoList = document.getElementById("todoList");
 
-btnAdd.onclick = function () {
-    // debugger
-    todoList.innerHTML = todoList.innerHTML + `<li  class="todo-list-item">${todoInput.value}</li>`
+// loading todo items from JS using DOM
+const arr = [
+    { task: "Milk", isCompleted: true },
+    { task: "Eggs", isCompleted: true },
+    { task: "Bread", isCompleted: false }
+];
+
+
+// a function that rerenders the todo list
+function renderList() {
+    //use a for loop to render the list items
+    var html = ""
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i].isCompleted) {
+            html += `<li class="todo-list-item completed" id=${i}>${arr[i].task}</li>`
+        } else {
+            html += `<li class="todo-list-item" id=${i}>${arr[i].task}</li>`
+        }
+    }
+    todoList.innerHTML = html
 }
+
+function strikeOff() {
+    var listItems = document.getElementsByClassName("todo-list-item")
+
+    for (let j = 0; j < listItems.length; j++) {
+        const item = listItems[j];
+
+        item.onclick = function () {
+            item.className = "todo-list-item completed";
+            arr[item.id].isCompleted = true;
+        }
+    }
+}
+
+
+btnAdd.onclick = function () {
+    arr.push({ task: todoInput.value, isCompleted: false });
+    renderList();
+    strikeOff();
+}
+
+renderList();
+strikeOff();
